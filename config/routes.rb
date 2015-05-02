79fc1e62eea104d81f2eac67558a314bbbfa0b43
chhavo <cadtani@gmail.com>
2015-05-02 11:05:14 -0500

    part2_complete

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,6 @@ Rails.application.routes.draw do
   # PART 2: VARIABLE ROUTES
   # =======================
 
-  # get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
+  get("/signs/:the_sign",         { :controller => "zodiacs", :action => "sign" })
   # get("/creatures/:the_creature", { :controller => "zodiacs", :action => "creature" })
 end
